86de09a04abd4289a3a658cf710125e3f615355c
Jonathan Ross <jonross@alum.mit.edu>
2014-03-01 14:38:39 -0500

    Add ArrayBuilder

---
+++ src/main/scala/trilby/util/ArrayBuilder.java
@@ -0,0 +1,54 @@
+package trilby.util;
+
+public class ArrayBuilder
+{
+    private ArrayBuilder() {}
+    
+    public static class OfInt {
+        
+        private int[] data;
+        private int size = 0;
+        
+        public OfInt(int initialCapacity) {
+            data = new int[initialCapacity];
+        }
+        
+        public int size() {
+            return size;
+        }
+        
+        public int[] add(int value) {
+            if (size == data.length) {
+                int[] newData = new int[(int) (data.length * 1.3)];
+                System.arraycopy(data, 0, newData, 0, data.length);
+                data = newData;
+            }
+            data[size++] = value;
+            return data;
+        }
+    }
+
+    public static class OfByte {
+        
+        private byte[] data;
+        private int size = 0;
+        
+        public OfByte(int initialCapacity) {
+            data = new byte[initialCapacity];
+        }
+        
+        public int size() {
+            return size;
+        }
+        
+        public byte[] add(byte value) {
+            if (size == data.length) {
+                byte[] newData = new byte[(int) (data.length * 1.3)];
+                System.arraycopy(data, 0, newData, 0, data.length);
+                data = newData;
+            }
+            data[size++] = value;
+            return data;
+        }
+    }
+}
